4ebc7c90c0f98e346d0ced042e6106e1cd09c809
Artur Faria <artur.faria.1992@gmail.com>
2024-07-01 10:48:52 -0300

    Section 6 - Lesson 171

---
+++ 06-spring-boot-spring-mvc/04-spring-mvc-add-data-to-model/src/test/java/br/com/projetospringboot3/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetospringboot3.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
