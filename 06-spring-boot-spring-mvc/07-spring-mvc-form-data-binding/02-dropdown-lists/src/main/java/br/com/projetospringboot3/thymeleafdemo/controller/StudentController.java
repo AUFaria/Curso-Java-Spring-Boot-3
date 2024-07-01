6f239f19b2a333e5f4d8b11cc5e7c352d3be2931
Artur Faria <artur.faria.1992@gmail.com>
2024-07-01 13:58:59 -0300

    Section 6 - Lesson 183

---
+++ 06-spring-boot-spring-mvc/07-spring-mvc-form-data-binding/02-dropdown-lists/src/main/java/br/com/projetospringboot3/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,38 @@
+package br.com.projetospringboot3.thymeleafdemo.controller;
+
+import br.com.projetospringboot3.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+	@Value("${countriesList}")
+	private List<String> countriesList;
+
+	@GetMapping("/showStudentForm")
+	public String showForm(Model model) {
+
+		Student student = new Student();
+
+		model.addAttribute("student", student);
+
+		model.addAttribute("countriesList", countriesList);
+
+		return "student-form";
+	}
+
+	@PostMapping("/processStudentForm")
+	public String processForm(@ModelAttribute("student") Student student) {
+
+		System.out.println("student: " + student.getFirstName() + " " + student.getLastName());
+
+		return "student-result";
+	}
+}
