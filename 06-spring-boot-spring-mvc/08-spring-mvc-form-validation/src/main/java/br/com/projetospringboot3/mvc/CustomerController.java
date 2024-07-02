b1927ce50839aa1ef2bf973f83577f821c68d5b2
Artur Faria <artur.faria.1992@gmail.com>
2024-07-02 13:51:07 -0300

    Revert "Section 6 - Lesson 199"
    This reverts commit 144c86da6e97d541f2daf77e649da0528ef27a9f.

--- 06-spring-boot-spring-mvc/08-spring-mvc-form-validation/src/main/java/br/com/projetospringboot3/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/08-spring-mvc-form-validation/src/main/java/br/com/projetospringboot3/mvc/CustomerController.java
@@ -1,26 +1,15 @@
 package br.com.projetospringboot3.mvc;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
-
-	@InitBinder
-	public void initBinder(WebDataBinder dataBinder) {
-
-		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
-
-		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
-	}
 
 	@GetMapping("/")
 	public String showForm(Model model) {
@@ -35,8 +24,6 @@ public class CustomerController {
 			@Valid @ModelAttribute("customer") Customer customer,
 			BindingResult bindingResult
 	) {
-
-		System.out.println("Last name: |" + customer.getLastName() + "|");
 
 		return bindingResult.hasErrors() ? "customer-form" : "customer-result";
 	}
