573bb6d2e37688f21de25594f3e68c7eb5caf7f0
Artur Faria <artur.faria.1992@gmail.com>
2024-07-03 12:25:48 -0300

    Section 7 - Lesson 223

---
+++ 07-spring-boot-spring-mvc-crud/02-spring-boot-mvc-crud-add-employees/src/main/java/br/com/projetospringboot3/crudmvc/CrudmvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetospringboot3.crudmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudmvcApplication.class, args);
+	}
+
+}
