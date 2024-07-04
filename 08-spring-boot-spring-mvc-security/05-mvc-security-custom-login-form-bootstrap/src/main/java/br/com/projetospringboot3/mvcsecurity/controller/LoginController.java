f5a61166450fea8b12357dc57dbc5fcc5cff19ea
Artur Faria <artur.faria.1992@gmail.com>
2024-07-04 11:33:14 -0300

    Section 8 - Lesson 243

---
+++ 08-spring-boot-spring-mvc-security/05-mvc-security-custom-login-form-bootstrap/src/main/java/br/com/projetospringboot3/mvcsecurity/controller/LoginController.java
@@ -0,0 +1,13 @@
+package br.com.projetospringboot3.mvcsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String showMyLoginPage() {
+		return "login";
+	}
+}
