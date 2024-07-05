05d345d14c2a1a09595c8666214c67d59447d424
Artur Faria <artur.faria.1992@gmail.com>
2024-07-05 15:44:24 -0300

    Section 9 - Lesson 291

---
+++ 09-spring-boot-jpa-hibernate-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/br/com/projetospringboot3/jpaadvmaps/JpaadvmapsApplication.java
@@ -0,0 +1,96 @@
+package br.com.projetospringboot3.jpaadvmaps;
+
+import br.com.projetospringboot3.jpaadvmaps.dao.AppDAO;
+import br.com.projetospringboot3.jpaadvmaps.entity.Instructor;
+import br.com.projetospringboot3.jpaadvmaps.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JpaadvmapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaadvmapsApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		return runner -> {
+//			createInstructor(appDAO);
+//		  getInstructor(appDAO);
+//		  deleteInstructor(appDAO);
+//		  getInstructorDetail(appDAO);
+		  deleteInstructorDetail(appDAO);
+		};
+	}
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+
+		int id = 3;
+
+		System.out.println("Deleting instructor detail with id: " + id);
+
+		appDAO.deleteInstructorDetailById(id);
+
+		System.out.println("Finished");
+
+	}
+
+	private void getInstructorDetail(AppDAO appDAO) {
+
+		int id = 2;
+
+		System.out.println("Finding instructor detail with id: " + id);
+
+		InstructorDetail instructorDetail = appDAO.findInstructorDetailById(id);
+
+		System.out.println("Instructor Detail: " + instructorDetail);
+		System.out.println("Instructor associated with Instructor Detail: " + instructorDetail.getInstructor());
+
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+
+		int id = 1;
+
+		System.out.println("Deleting instructor with id: " + id);
+
+		appDAO.deleteInstructorById(id);
+
+		System.out.println("Finished");
+	}
+
+	private void getInstructor(AppDAO appDAO) {
+
+		int id = 2;
+
+		System.out.println("Finding instructor with id: " + id);
+
+		Instructor instructor = appDAO.findInstructorById(id);
+
+		System.out.println("Instructor: " + instructor);
+		System.out.println("Instructor Detail associated with Instructor: " + instructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+//		Instructor instructor = new Instructor("Joao", "Silva", "joao.silva@email.com");
+//
+//		InstructorDetail instructorDetail = new InstructorDetail("youtube.com/joao", "Natação");
+
+		Instructor instructor = new Instructor("Pedro", "Henrique", "pedro.henrique@email.com");
+
+		InstructorDetail instructorDetail = new InstructorDetail("youtube.com/pedrohenrique", "Volei");
+
+		instructor.setInstructorDetail(instructorDetail);
+
+		System.out.println("Saving instructor: " + instructor);
+
+		appDAO.save(instructor);
+
+		System.out.println("Finished");
+	}
+}
